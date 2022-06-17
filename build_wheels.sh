c38104edd4a33cb55d2dcdbfb3353f239ab85935
joncrall <jon.crall@kitware.com>
2022-06-17 10:39:26 -0400

    Fix publish

--- build_wheels.sh
+++ build_wheels.sh
@@ -3,5 +3,6 @@ __doc__="""
 SeeAlso:
     pyproject.toml
 """
-pip wheel -w wheelhouse .
-#cibuildwheel --config-file pyproject.toml --platform linux --arch x86_64
+#pip wheel -w wheelhouse .
+python -m build --wheel -o wheelhouse  #  xcookie: +COMMENT_IF(binpy)
+#cibuildwheel --config-file pyproject.toml --platform linux --arch x86_64  #  xcookie: +UNCOMMENT_IF(binpy)
