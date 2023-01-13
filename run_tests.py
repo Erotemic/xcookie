b36bc810d66c9631e37d6fdc58dce8a369cbb389
joncrall <erotemic@gmail.com>
2023-01-13 15:24:17 -0500

    wip

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import pytest
     import sys
     package_name = 'xcookie'
-    mod_dpath = package_name
+    mod_dpath = 'xcookie'
     test_dpath = 'tests'
     pytest_args = [
         '--cov-config', 'pyproject.toml',
