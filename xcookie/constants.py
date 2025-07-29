f126df92036a8567344751f97b1a63c87dbc5b09
joncrall <erotemic@gmail.com>
2025-07-29 10:36:46 -0400

    temporary revert 3.14

--- xcookie/constants.py
+++ xcookie/constants.py
@@ -2,7 +2,8 @@ from typing import List, Dict
 
 KNOWN_PYTHON_VERSIONS: List[str]  = [
     '2.7', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '3.10', '3.11', '3.12',
-    '3.13', '3.14'
+    '3.13',
+    # '3.14'
 ]
 
 DEV_PYTHON_VERSIONS: List[str] = [
